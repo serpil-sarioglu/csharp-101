cd319a9c80b125ab03a362b5b7181f47eb47c296
Serpil <serpilsarioglu81@gmail.com>
2021-09-28 10:16:22 +0300

    konsol projesi olusturuldu
    kullanıcı girdisi ile konsol ekranında merhaba yazdırıldı

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadnızı Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
