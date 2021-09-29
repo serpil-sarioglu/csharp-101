d75ad5bd0d6154b46761e59943cd1900fe2caaa3
Serpil <serpilsarioglu81@gmail.com>
2021-09-29 22:44:24 +0300

    if-else-if konsol projesi oluşturuldu
    if -else if-else , ternary if  karar yapıları 1 dersi tamamlandı

---
+++ if-else-if/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Geceler!");
+
+            string sonuc = time <= 18 ? "Merhaba, İyi Günler!" : "Merhaba, İyi Geceler!";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın :D" : time <= 18 ? "İyi Günler :D" : "İyi Geceler :D";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
