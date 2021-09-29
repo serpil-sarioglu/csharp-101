0292671c5b930fa1f6452b453cdf298f0f30784c
Serpil <serpilsarioglu81@gmail.com>
2021-09-29 23:24:28 +0300

    koşullu ifadeler challenge yapıldı

--- if-else-if/Program.cs
+++ if-else-if/Program.cs
@@ -19,6 +19,20 @@ namespace if_else_if
             sonuc = time >= 6 && time < 11 ? "Günaydın :D" : time <= 18 ? "İyi Günler :D" : "İyi Geceler :D";
             Console.WriteLine(sonuc);
 
+            //Hackkerrank|Day 3: Intro to Conditional Statements: N sayısının tek olma durumlarında weird çift olma durumlarında not weird yazılmasını sağlayan kodu yazınız.
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N % 2 != 0)
+                Console.WriteLine("Weird");
+            else
+            {
+                if (N >= 2 && N <= 5)
+                    Console.WriteLine("Not Weird");
+                else if (N >= 6 && N <= 20)
+                    Console.WriteLine("Weird");
+                else if (N > 20)
+                    Console.WriteLine("Not Weird");
+            }
+
         }
     }
 }
