3c4d2c13fb75fa3645106a7a383f6aaf9de96794
Serpil <serpilsarioglu81@gmail.com>
2021-10-15 01:36:04 +0300

    hackerrank challenge yapıldı

--- diziler-array-sinifi-metodlari/Program.cs
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System;
 namespace diziler_array_sinifi_metodlari
 {
     class Program
@@ -48,18 +51,55 @@ namespace diziler_array_sinifi_metodlari
             foreach (var sayi in sayiDizisi)
                 Console.WriteLine(sayi);
 
-            // HackerRank Challenge| Birthday Cake Candles 
-            // Girilen dizideki en büyük elamandan kaç tane olduğunu bulup sayısını döndüren program
-            Console.Write("Lütfen dizi boyutunu giriniz: ");
-            int candlesCount = Convert.ToInt32(Console.ReadLine());
-            int[] candles = new int[candlesCount];
-            for (int i = 0; i < candlesCount; i++)
+            // // HackerRank Challenge| Birthday Cake Candles 
+            // // Girilen dizideki en büyük elamandan kaç tane olduğunu bulup sayısını döndüren program
+            // Console.Write("Lütfen dizi boyutunu giriniz: ");
+            // int candlesCount = Convert.ToInt32(Console.ReadLine());
+            // int[] candles = new int[candlesCount];
+            // for (int i = 0; i < candlesCount; i++)
+            // {
+            //     Console.Write("Lütfen {0}. sayıyı giriniz: ", i + 1);
+            //     candles[i] = Convert.ToInt32(Console.ReadLine());
+            // }
+            // int result = Result.birthdayCakeCandles(candles);
+            // Console.WriteLine(result);
+
+            // // HackerRank Challenge| Simple Array Sum 
+            // Console.Write("Lütfen dizi boyutunu giriniz: ");
+            // int arCount = Convert.ToInt32(Console.ReadLine());
+            // int[] ar = new int[arCount];
+            // for (int i = 0; i < arCount; i++)
+            // {
+            //     Console.Write("Lütfen {0}. sayıyı giriniz: ", i + 1);
+            //     ar[i] = Convert.ToInt32(Console.ReadLine());
+            // }
+            // int result = Result.simpleArraySum(ar);
+            // Console.WriteLine(result);
+
+            // // HackerRank Challenge| Compare the Triplets 
+            // // https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+            Console.Write("Lütfen listenin boyutunu giriniz: ");
+            int listCount = Convert.ToInt32(Console.ReadLine());
+            List<int> a = new List<int>();
+            List<int> b = new List<int>();
+            for (int i = 0; i < listCount; i++)
             {
-                Console.Write("Lütfen {0}. sayıyı giriniz: ", i + 1);
-                candles[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Lütfen a listesi için {0}. sayı giriniz: ", i + 1);
+                a.Add(Convert.ToInt32(Console.ReadLine()));
             }
-            int result = Result.birthdayCakeCandles(candles);
-            Console.WriteLine(result);
+            for (int i = 0; i < listCount; i++)
+            {
+                Console.WriteLine("Lütfen b listesi için {0}. sayıyı giriniz: ", i + 1);
+                b.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            List<int> result = Result.compareTriplets(a, b);
+            Console.WriteLine("SCORE:  [a]" + "\t" + "[b]");
+            foreach (var item in result)
+            {
+                Console.Write("\t" + "[" + item + "]");
+            }
+
         }
     }
     class Result
@@ -81,7 +121,32 @@ namespace diziler_array_sinifi_metodlari
                     count++;
             }
             return count;
+        }
+        public static int simpleArraySum(int[] ar)
+        {
+            int sum = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+            return sum;
+        }
 
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int alice = 0;
+            int bob = 0;
+            List<int> answer = new List<int>();
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                    alice++;
+                if (a[i] < b[i])
+                    bob++;
+            }
+            answer.Insert(0, alice);
+            answer.Insert(1, bob);
+            return answer;
         }
     }
 }
