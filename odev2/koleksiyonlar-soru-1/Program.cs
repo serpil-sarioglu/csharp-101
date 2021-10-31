09684c1c206876e273b4cd26398c4907f9b9a31e
Serpil <serpilsarioglu81@gmail.com>
2021-10-31 23:38:10 +0300

    koleksiyonlar-soru-1 ödevi güncellendi

--- odev2/koleksiyonlar-soru-1/Program.cs
+++ odev2/koleksiyonlar-soru-1/Program.cs
@@ -5,6 +5,10 @@ namespace koleksiyonlar_soru_1
 {
     class Program
     {
+        // Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+        // Negatif ve numeric olmayan girişleri engelleyin.
+        // Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+        // Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
         static void Main(string[] args)
         {
 
