2b2b47b0cc4da30ba05ce5c1f8308ed13237a358
Serpil <serpilsarioglu81@gmail.com>
2022-01-22 16:04:06 +0300

    sinif-kavrami konsol projesine ait hackerrank challenge ödevi yapıldı

--- sinif-kavrami/Program.cs
+++ sinif-kavrami/Program.cs
@@ -40,6 +40,23 @@ namespace sinif_kavrami
             calisan2.Departman = "Pazarlama";
             calisan2.CalisanBilgileri();
 
+            // Hackerrank Challenge | 
+            // T: test senayo sayısıdır. Girilen yaş argümanına göre kontrol ve metotlara ait işlem yaptırılır.
+            Console.Write("Test senaryo sayısını giriniz: ");
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                Console.Write("Yaşınızı giriniz: ");
+                int age = int.Parse(Console.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+
+                for (int j = 0; j < 3; j++)
+                    p.yearPasses();
+
+                p.amIOld();
+                Console.WriteLine();
+            }
         }
     }
 
@@ -55,6 +72,36 @@ namespace sinif_kavrami
             Console.WriteLine("Çalışan Soyadı: {0}", Soyad);
             Console.WriteLine("Çalışan Numarası: {0}", No);
             Console.WriteLine("Çalışan Departmanı: {0}", Departman);
+        }
+    }
+
+    class Person
+    {
+        public int age;
+        public Person(int initialAge)
+        {
+            if (initialAge < 0)
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+            else
+                age = initialAge;
+        }
+
+        public void amIOld()
+        {
+            if (age < 13)
+                Console.WriteLine("You are young.");
+            else if (age >= 13 && age < 18)
+                Console.WriteLine("You are a teenager.");
+            else
+                Console.WriteLine("You are old.");
+        }
+
+        public void yearPasses()
+        {
+            age = age + 1;
         }
     }
 }
